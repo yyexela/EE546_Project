37865239bd63778b112b5df72ef0040fe414669a
Alexey Y <yyexela@disroot.org>
2025-02-06 17:06:34 -0800

    Started working on book

--- EE546_Project/EE546Project/Basic.lean
+++ EE546_Project/EE546Project/Basic.lean
@@ -1,1 +1,39 @@
-def hello := "world"
+/-
+
+  An Introduction to Mathematical Cryptography
+  (Second Edition)
+
+  Jeffrey Hoffstein
+  Jill Pipher
+  Joseph H. Silverman
+
+  Lean code created by
+
+  Henry Do
+  Alexey Yermakov
+
+-/
+
+import Mathlib.Algebra.Divisibility.Basic
+import Mathlib.Tactic
+open Classical
+
+/-
+  Chapter 1: An Introduction to Cryptography
+-/
+
+theorem prop1_4_a {a b c : ℤ} : a ∣ b → b ∣ c → a ∣ c := by
+  intro h1 h2
+  exact Int.dvd_trans h1 h2
+
+theorem prop1_4_b {a b : ℤ} : a ∣ b → b ∣ a → a = b ∨ a = -b := by
+  intro h1 h2
+  simp_all[dvd_def]
+  obtain ⟨c, h1⟩ := h1
+  obtain ⟨d, h2⟩ := h2
+  rw[h1] at h2
+  have : 1 = c*d := by match a with
+  | 0 => sorry
+  | ofNat x => sorry
+  | negSucc x => sorry
+  sorry
