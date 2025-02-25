ddb723f7f060747c2d65f20efebcda730f875ee0
Alexey Y <yyexela@disroot.org>
2025-02-25 11:30:40 -0800

    Finished rings for Thursday

--- EE546_Project/EE546Project/Basic.lean
+++ EE546_Project/EE546Project/Basic.lean
@@ -396,16 +396,10 @@ namespace my_rings
   /-
   Integer rings
 
-  Note: mathlib definition:
-
-  def ZMod : ℕ → Type
-    | 0 => ℤ
-    | n + 1 => Fin (n + 1)
-
-  "Ring of integers modulo m"
+  Goal: "Ring of integers modulo m"
   -/
 
-  -- Start with simple definition
+  -- Start with simple definition - ring mod 5
   structure IntRingMod5 where
     elements : Fin 5
     deriving Repr  -- For custom prints
@@ -417,13 +411,98 @@ namespace my_rings
   def a : IntRingMod5 := 4 -- OfNat required
   def b : Fin 5 := 4
 
-  -- Require [NeZero n] so that elements is not empty
-  instance (n : ℕ) [NeZero n] : OfNat IntRingMod5 n where
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  -- Define OfNat
+  instance (n : ℕ) : OfNat IntRingMod5 n where
     ofNat := { elements := Fin.ofNat' 5 n }
 
   def c : IntRingMod5 := 4
-  #eval c + c -- define addition
-  #eval c * c -- define multiplication
+  #eval c + c -- need to define addition
+  #eval c * c -- need to define multiplication
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  -- Define add and multiply
+  #eval c + c
+  #eval c * c
 
   -- Readable way
   def IntRingMod5.add (a b : IntRingMod5) : IntRingMod5 :=
@@ -436,11 +515,97 @@ namespace my_rings
   def IntRingMod5.mul (a b : IntRingMod5) : IntRingMod5 := match a, b with
   | ⟨x⟩, ⟨y⟩ => ⟨x * y⟩
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   -- Show finset handles modulo already
   #eval IntRingMod5.add ⟨2⟩ ⟨3⟩  -- Output: ⟨0⟩ (since 2 + 3 = 5 ≡ 0 mod 5)
-  #eval IntRingMod5.mul ⟨2⟩ ⟨3⟩  -- Output: ⟨0⟩ (since 2 + 3 = 5 ≡ 0 mod 5)
+  #eval IntRingMod5.mul ⟨2⟩ ⟨3⟩  -- Output: ⟨0⟩ (since 2 * 3 = 6 ≡ 1 mod 5)
 
+  -- not quite finished!
   #eval c + c
+  #eval c * c
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   -- Define HAdd
   instance : HAdd IntRingMod5 IntRingMod5 IntRingMod5 where
@@ -460,24 +625,202 @@ namespace my_rings
 
 
 
-  -- Start with simple definition
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  -- General ring mod m
   def IntRingModM : ℕ → Type
     | 0 => ℤ
     | n + 1 => Fin (n + 1)
 
-  -- Overwrite print
-  instance : Repr IntRingMod5 where
-    reprPrec r _ := repr r.elements
-
   def a1 : IntRingModM 5 := 4 -- OfNat required
 
-  -- Require [NeZero n] so that elements is not empty
   instance (n m : ℕ) : OfNat (IntRingModM m) n where
     ofNat :=  match m with
-    | 0 => ℤ
-    | Nat.succ x => ⟨x, by exact lt_add_one x⟩
+    | 0 => Int.ofNat n
+    | Nat.succ x => Fin.ofNat' (Nat.succ x) n
 
   def c1 : IntRingModM 5 := 4
+  #eval c1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  -- Define add and multiply
+
+  -- Readable way
+  def IntRingModM.add {m : ℕ} (a b : IntRingModM m) : IntRingModM m := match m with
+  | 0 => Int.add a b
+  | Nat.succ _ => Fin.add a b
+
+  -- Readable way
+  def IntRingModM.mul {m : ℕ} (a b : IntRingModM m) : IntRingModM m := match m with
+  | 0 => Int.mul a b
+  | Nat.succ _ => Fin.mul a b
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  -- Make lean parse understand "+" and "*"
+
+  -- Define HAdd
+  instance {m : ℕ} : HAdd (IntRingModM m) (IntRingModM m) (IntRingModM m) where
+    hAdd a b := IntRingModM.add a b
+
+  -- Define HAdd
+  instance {m : ℕ} : HMul (IntRingModM m) (IntRingModM m) (IntRingModM m) where
+    hMul a b := IntRingModM.mul a b
+
+  #eval c1
+  #eval c1 + c1 -- 4 + 4 = 8 = 3 + 5 = 3 mod 5
+  #eval c1 * c1 -- 4 * 4 = 16 = 1 + 15 = 1 mod 5
+
+  #eval (2 : (IntRingModM 0)) + (8 : (IntRingModM 0))
+  #eval (2 : (IntRingModM 10)) + (8 : (IntRingModM 10))
+  #eval (2 : (IntRingModM 10)) * (8 : (IntRingModM 10))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
